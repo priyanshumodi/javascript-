f424bd344810097f87cd0cc80a72a601b2b81336
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-01-09 18:33:19 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,37 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ********************************** Operations *****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "radhe radhe"
+let str2 = " krishna"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"")
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2 //its correct but should not use in production
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
