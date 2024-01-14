fbf6ef1888207316ca117baad631ea47863c224e
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-01-14 17:32:06 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,9 +20,9 @@ function addTwoNumbers(num1,num2)
     return num1+num2;
 }
 let result = addTwoNumbers(3,4);
-console.log(result);
+// console.log(result);
 
-function loginUserMessage(username="radha")
+function loginUserMessage(username="radha") // argument pass nhi kiya to radha
 {
     if(!username)
     {
@@ -31,4 +31,32 @@ function loginUserMessage(username="radha")
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("krishna"));
+// console.log(loginUserMessage("krishna"));
+
+function calculateCartPrice(val1,val2,...num1) { // rest and spread orperator
+    return num1;
+}
+console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username: "Krishna",
+    prices: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.prices}`);
+
+}
+// handleObject(user);
+handleObject({
+    username: "priyanshu",
+    prices: 19
+})
+
+const myArray = [100,200,300]
+
+function returnSecondValue(arr){
+    return arr[1]
+}
+// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([10,20,30]));
