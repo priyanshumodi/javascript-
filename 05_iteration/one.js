65a70db5a297fc68ec6b0a1ce0ee2c49bb32d8ec
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-01-23 18:45:41 +0000

    basic loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,19 @@
+// for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(i + '*' + j + '');
+// }
+
+let arr = ["krishna","radha","sudama"]
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index] + ' ');
+    
+}
+
+// break and continue
+
+// you knew it
+
+
---
+++ 05_iteration/two.js
@@ -0,0 +1,8 @@
+//  while
+
+// you knew it 
+
+// do while 
+
+// at least one time run
+
