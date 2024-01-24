6cc9871dca12f35bd108294d36026155b70f7b34
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-01-24 18:46:13 +0000

    filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,38 @@
+const coding = ["java","c","cpp","python"]
+
+
+// const value = coding.forEach( (item) => {
+//     console.log(item);
+//     return item;
+// })
+
+// console.log(value);
+
+// filter is used for return a data of for
+// const value = coding.filter( (item) => {
+//     console.log(item);
+//     return item;
+// })
+// console.log(value);
+
+// const arr = []
+// coding.forEach( (item)=> {
+//     arr.push(item);
+// })
+// console.log(arr);
+
+const book = [
+    {
+        title: 'Book Six',genre : 'fiction',publish: 1987
+    },
+    {
+        title: 'Book Seven',genre : 'history',publish: 2000
+    },
+    {
+        title: 'Book *',genre : 'science',publish: 2002
+    }
+]
+
+const userBook = book.filter( (bk) => bk.genre==='history')
+
+console.log(userBook);
